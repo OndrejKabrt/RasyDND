776f08569b4ffaf25bab53d459a9dfc76207ac4d
OndrejKabrt <kabrt@spsejecna.cz>
2024-04-17 08:19:24 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- použito, z kodu pana učitele Pavláta -->
+
+<?php
+session_start();
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '':
+    case '/':
+        $redirect = '\OtherSites\index.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '\OtherSites\404.php';
+        exit();
+}
+
+$_SESSION['site'] = $redirect;
+require_once __DIR__ . '/OtherSites/basic/Header.php';
+require_once __DIR__ . $redirect ?? __DIR__ . '/OtherSites/index.php';
+require_once __DIR__ . '/OtherSites/basic/Footer.php';
+
+
